7eb47ab0a7c5729b7dac795ff962f96cb0f2a542
Alexander Brown <alexanderbrown.info@gmail.com>
2016-10-26 20:05:41 +0100

    added gitignore

---
+++ test/calculator_test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+const calc = require('../calculator/calculator.js'); // this is the path to our calculator.js file
+
+
+describe('Calculator Tests', function() {
+
+    it('it should return zero when passed in an empty string', function(done) {
+        assert.equal(calc.stringCalculator(''), 0); // the empty string is what we pass the function and the zero is the expected output
+        done();
+    });
+});
